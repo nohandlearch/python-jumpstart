4e00e2de8023c306892cf08781cb45f99799b4d4
nohandlearch <no.handle.arch@gmail.com>
2018-01-17 22:11:21 -0600

    Add number guessing game

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # python-jumpstart
 Repo for apps developed during Python Jumpstart training from TalkPython.fm
 
-App #1: Hello world
+App #1: Hello world (hello_world.py)
+App #2: Number guessing game (number_guess.py)
---
+++ number_guess.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Number guessing game"""
+
+import random
+
+separator = '--------------------------------'
+
+print(separator)
+print('       GUESS THE NUMBER')
+print(separator)
+
+number = random.randint(0, 100)
+
+guess_text = input('Guess a number between 0 and 100: ')
+guess = int(guess_text)
+
+while guess != number:
+    if guess < number:
+        print('Sorry, your guess of {} was too low.'.format(guess))
+    elif guess > number:
+        print('Sorry, your guess of {} was too high.'.format(guess))
+    
+    guess_text = input('Guess again: ')
+    guess = int(guess_text)
+
+print('Nailed it! You win!')
