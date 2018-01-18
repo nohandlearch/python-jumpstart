10e9a43a33ee8ae75466ec3de4f3371ed85dbfc4
nohandlearch <no.handle.arch@gmail.com>
2018-01-17 21:10:15 -0600

    Add Hello World app

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # python-jumpstart
 Repo for apps developed during Python Jumpstart training from TalkPython.fm
+
+App #1: Hello world
---
+++ hello_world.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Hello world application. Prints the name received from user input"""
+
+separator = '--------------------------------'
+
+print(separator)
+print('           HELLO WORLD')
+print(separator)
+
+name = input('What is your name? ')
+print('Nice to meet you, {0}'.format(name))
